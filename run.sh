a0f6aa053aacc7fcd2ca8a79a6835150afcde487
PajE <paje@github.com>
2020-05-31 21:46:49 +0200

    correction run.sh

--- run.sh
+++ run.sh
@@ -10,9 +10,7 @@ docker build .
 
 
 docker run \
-	--name ${ICQBOT_NAME} \
 	--mount type=bind,source=${WORKDIR}/log/,target=/work/log/ \
 	--mount type=bind,source=${WORKDIR}/db/,target=/work/db/ \
 	--mount type=bind,source=${WORKDIR}/tmp/,target=/work/tmp/ \
-	--rm \
-	-ti ${ICQBOT_NAME}:${ICQBOT_VERSION} 
+	--rm
